c2916b56d29853b0bc2072896077ee2cc8af1176
mayuri <64832914+mayuri21-design@users.noreply.github.com>
2021-06-19 15:55:35 +0530

    Update urls.py

--- commerce/urls.py
+++ commerce/urls.py
@@ -24,5 +24,4 @@ from django.conf.urls import url
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("auctions.urls"))
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
